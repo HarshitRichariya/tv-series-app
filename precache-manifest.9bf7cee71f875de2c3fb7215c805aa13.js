bc4c82fa4b88f46fe9c9f626f448d5e005b22fa7
Harshit Richariya <harshitrichariya77@gmail.com>
2019-11-27 01:02:09 +0530

    Updates

---
+++ precache-manifest.9bf7cee71f875de2c3fb7215c805aa13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655ef3eeb52e8b0c26d29aa0eb224460",
+    "url": "/tv-series-app/index.html"
+  },
+  {
+    "revision": "7964671cb79d627e9170",
+    "url": "/tv-series-app/static/css/main.11dd20b4.chunk.css"
+  },
+  {
+    "revision": "5fc3cf14299ae4906abf",
+    "url": "/tv-series-app/static/js/2.993a46f6.chunk.js"
+  },
+  {
+    "revision": "7964671cb79d627e9170",
+    "url": "/tv-series-app/static/js/main.9efb3150.chunk.js"
+  },
+  {
+    "revision": "9fe73fa7110a98658a2e",
+    "url": "/tv-series-app/static/js/runtime~main.1839de02.js"
+  }
+]);
